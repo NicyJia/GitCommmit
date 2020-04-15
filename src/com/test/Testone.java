b31a33606bd626f74baf82fa7ae457d43f447555
Administrator <Administrator@192.168.75.1>
2020-04-15 22:41:21 +0800

    1

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -9,6 +9,7 @@ public class Testone {
 	public static void main(String[] args) {
 
 		System.out.println("qqqqqq");
+		System.out.println("nihaoa");
 		HashAttributeSet hashAttributeSet = new HashAttributeSet();
 		hashAttributeSet.toString();
 		System.out.println("nihao");
