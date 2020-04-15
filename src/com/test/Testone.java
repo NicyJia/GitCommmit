b58070e729c4a9cd5ef72c111f68c490027c705c
Administrator <Administrator@192.168.75.1>
2020-04-15 22:25:12 +0800

    1

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -3,10 +3,14 @@ package com.test;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.print.attribute.HashAttributeSet;
+
 public class Testone {
 	public static void main(String[] args) {
 
 		System.out.println("Ïë²âÊÔÏÂ³åÍ»");
+		HashAttributeSet hashAttributeSet = new HashAttributeSet();
+		hashAttributeSet.toString();
 
 	}
 }
