4602ed9c4c9cd2fb82c15514897584a913ecfe6b
Administrator <Administrator@192.168.75.1>
2020-04-15 22:21:41 +0800

    1

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -7,12 +7,6 @@ public class Testone {
 	public static void main(String[] args) {
 
 		System.out.println("想测试下冲突");
-		List<Object> arrayList = new ArrayList<Object>();
-		arrayList.add("只是一个测试赛");
-
-		System.out.println("虏芒脢脭鲁氓脥禄");
-		System.out.println("111");
-		System.out.println("娴嬭瘯鍐茬獊");
 
 	}
 }
