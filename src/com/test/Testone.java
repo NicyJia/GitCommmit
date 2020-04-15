896991f381d68a43618f46cf0ba11aa7621f26f5
BrigntSun <31766522+NicyJia@users.noreply.github.com>
2020-04-15 21:55:28 +0800

    Update Testone.java

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -2,6 +2,9 @@ package com.test;
 
 public class Testone {
 	public static void main(String[] args) {
-      System.out.println("���Գ�ͻ");
+      System.out.println("²âÊÔ³åÍ»");
+		  System.out.println("²âÊÔ³åÍ»");
+		  System.out.println("²âÊÔ³åÍ»");
+		  System.out.println("²âÊÔ³åÍ»");
 	}
 }
