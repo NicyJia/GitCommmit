33def3efeddc4ee2a924ac0941b6b1630fccba6c
Administrator <Administrator@192.168.75.1>
2020-04-15 22:12:39 +0800

    测试

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -1,10 +1,14 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Testone {
 	public static void main(String[] args) {
-      System.out.println("测试冲突");
-      System.out.println("111");
-	  System.out.println("111");
-	  System.out.println("111");
+
+		System.out.println("想测试下冲突");
+		List<Object> arrayList = new ArrayList<Object>();
+		arrayList.add("只是一个测试赛");
+
 	}
 }
