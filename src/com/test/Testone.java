8dbb64693e9cc0674fe86d51777815f562893b8c
BrigntSun <31766522+NicyJia@users.noreply.github.com>
2020-04-15 22:23:56 +0800

    Update Testone.java
    你好

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class Testone {
 	public static void main(String[] args) {
 
-		System.out.println("Ïë²âÊÔÏÂ³åÍ»");
+		System.out.println("nihao");
+		List list = new ArrayList<String>();
 
 	}
 }
