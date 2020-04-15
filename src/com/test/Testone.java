dab124e51962882fd0992681283c5e6dd18313b3
BrigntSun <31766522+NicyJia@users.noreply.github.com>
2020-04-15 22:32:55 +0800

    Update Testone.java
    q

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -8,7 +8,7 @@ import javax.print.attribute.HashAttributeSet;
 public class Testone {
 	public static void main(String[] args) {
 
-		System.out.println("Ïë²âÊÔÏÂ³åÍ»");
+		System.out.println("nihaoa");
 		HashAttributeSet hashAttributeSet = new HashAttributeSet();
 		hashAttributeSet.toString();
 		System.out.println("nihao");
