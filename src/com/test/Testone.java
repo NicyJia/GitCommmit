be93ad2c3d42e2d6c6bbe1cfcdee9144f3453ccf
Administrator <Administrator@192.168.75.1>
2020-04-15 22:01:31 +0800

    1

--- src/com/test/Testone.java
+++ src/com/test/Testone.java
@@ -3,6 +3,10 @@ package com.test;
 public class Testone {
 	public static void main(String[] args) {
       System.out.println("²âÊÔ³åÍ»");
+      System.out.println("²âÊÔ³åÍ»");
+      System.out.println("²âÊÔ³åÍ»");
+      System.out.println("²âÊÔ³åÍ»");
+      System.out.println("²âÊÔ³åÍ»");
       System.out.println("111");
 	}
 }
